e8419b894905e9c27e37a66fc1a5efc735538e1a
Romain Calmelet <romaincalmelet@gmail.com>
2026-04-30 06:32:20 +0200

    update2

--- nextjs-base/src/lib/stripe.ts
+++ nextjs-base/src/lib/stripe.ts
@@ -8,7 +8,7 @@ export function getStripe(): Stripe {
   }
   if (!_stripe) {
     _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2026-03-25.dahlia',
+      apiVersion: '2026-04-22.dahlia',
       typescript: true,
     })
   }
