a839bc1ae60a92fe1ebc3e29762a589d237991e6
Romain Calmelet <romaincalmelet@gmail.com>
2026-05-28 14:54:33 +0200

    signeOut

--- nextjs-base/src/components/espace-client/logout.ts
+++ nextjs-base/src/components/espace-client/logout.ts
@@ -1,9 +1,6 @@
-import { signOut } from 'next-auth/react'
-
 type PerformLogoutOptions = {
   locale: string
   resetSession?: () => Promise<unknown>
-  signOutAction?: typeof signOut
   navigate?: (url: string) => void
 }
 
@@ -22,19 +19,12 @@ function defaultNavigate(url: string) {
 export async function performLogout({
   locale,
   resetSession = defaultResetSession,
-  signOutAction = signOut,
   navigate = defaultNavigate,
 }: PerformLogoutOptions) {
-  const callbackUrl = `/${locale}`
+  const targetUrl = `/${locale}`
 
   await resetSession().catch(() => undefined)
 
-  const result = await signOutAction({
-    redirect: false,
-    callbackUrl,
-  }).catch(() => null)
-
-  const targetUrl = result?.url ?? callbackUrl
   navigate(targetUrl)
 
   return targetUrl
